4a57accbfddd02471f73ed18279e737acbfbf7a9
Rushikesh28 <rushikamod2002@gmail.com>
2025-04-03 15:28:24 +0530

    All calculation added

--- add.java
+++ add.java
@@ -4,12 +4,11 @@ public class ArithmeticOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Input two numbers
-
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
         System.out.print("Enter second number: ");
         double num2 = scanner.nextDouble();
-        // Calculate sum and difference
+
         double sum = num1 + num2;
         double difference = num1 - num2;
 	double product = num1 * num2;
